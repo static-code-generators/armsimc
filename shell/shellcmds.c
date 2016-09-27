6913e74eff9deaf5eafea79a84970308a2a0c715
Jai Luthra <me@jailuthra.in>
2016-09-28 02:47:09 +0530

    Add skeleton simulator code

--- shell/shellcmds.c
+++ shell/shellcmds.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "shellcmds.h"
 
 void cmd_run(){}
@@ -6,5 +7,16 @@ void cmd_step(int nbstep){}
 void cmd_mdump(int low_addr, int high_addr, char *fname){}
 void cmd_rdump(char *fname){}
 void cmd_set(int reg_num, int reg_val){}
-void cmd_help(){}
+
+void cmd_help()
+{
+	printf("`r` or `run`: simulate the program until it indicates that the simulator should halt.\n");
+	printf("`file <hexfile>`: load this file in program memory.\n");
+	printf("`step [i]`: execute one instruction (or optionally `i`)\n");
+	printf("`mdump <low> <high> [dumpfile]`: dump the contents of memory, from location low to location high to the screen or to the dump file [dumpfile].\n");
+	printf("`rdump [dumpfile]`: dump the current instruction count, the contents of R0 – R14, R15 (PC), and the CPSR to the screen or to the file [dumpfile].\n");
+	printf("`set <reg_num> <reg_val>`: set general purpose register reg num to value reg val.\n");
+	printf("`?` or `help`: print out a list of all shell commands.\n");
+	printf("`q` or `quit`: quit the shell.\n");
+}
 
---
+++ shell/sim.c
@@ -0,0 +1,50 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#define NB_REGS 16
+
+struct CPUState {
+	uint32_t regs[NB_REGS]; ///> Register File
+	uint32_t CPSR; ///> Current Program Status Register
+};
+
+/* We are not implementing stack ops right now */
+#define MEM_TEXT_START 0x00000000
+#define MEM_TEXT_SIZE  0x00100000
+#define MEM_DATA_START 0x20000000
+#define MEM_DATA_SIZE  0x00100000
+
+#define NB_REGIONS 2
+#define MEM_TEXT 0
+#define MEM_DATA 1
+
+struct MemoryRegion {
+	uint32_t start;
+	uint32_t size;
+	uint8_t *mem;
+};
+
+
+/* Global state variables */
+struct CPUState cpu_state;
+struct MemoryRegion mem_region[NB_REGIONS] = {
+	{MEM_TEXT_START, MEM_TEXT_SIZE, NULL},
+	{MEM_DATA_START, MEM_DATA_SIZE, NULL},
+};
+
+static void mem_init()
+{
+	int i;
+	for (i = 0; i < NB_REGIONS; i++) {
+		mem_region[i].mem = malloc(sizeof(uint8_t) * mem_region[i].size);
+	}
+}
+
+static void reset_cpu()
+{
+	int i;
+	cpu_state.CPSR = 0x0;
+	for (i = 0; i < NB_REGS; i++) {
+		cpu_state.regs[i] = 0x00;	
+	}
+}
