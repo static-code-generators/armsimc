2bbac1016a3e70198b628bda09f9b86aafb05f87
Jai Luthra <me@jailuthra.in>
2016-09-29 00:30:30 +0530

    simulator: add mem r/w and program load functions

--- shell/sim.c
+++ shell/sim.c
@@ -27,27 +27,71 @@ struct MemoryRegion {
 
 
 /* Global state variables */
-struct CPUState cpu_state;
+struct CPUState state_current, state_next;
 struct MemoryRegion mem_region[NB_REGIONS] = {
 	{MEM_TEXT_START, MEM_TEXT_SIZE, NULL},
 	{MEM_DATA_START, MEM_DATA_SIZE, NULL},
 };
 
-/** Allocate memory for memory. */
-static void mem_init()
+/** Allocate memory, initialze CPU states */
+void initialize()
 {
 	int i;
+    reset_cpu();
 	for (i = 0; i < NB_REGIONS; i++) {
 		mem_region[i].mem = malloc(sizeof(uint8_t) * mem_region[i].size);
+        memset(mem_region[i].mem, 0, sizeof(uint8_t) * mem_region[i].size);
+	}
+    state_next = state_current;
+}
+
+/** Load program into memory */
+void load_program(FILE *code)
+{
+    uint32_t word;
+    int i = 0;
+    while (fscanf(code, "%x\n", &word) != EOF) {
+        mem_write_32(MEM_TEXT_START + i, word);
+        i += 4;
+    }
+}
+
+/** Set all registers to 0 */
+void reset_cpu()
+{
+	int i;
+	state_current.CPSR = 0x00;
+	for (i = 0; i < NB_REGS; i++) {
+		state_current.regs[i] = 0x00;
 	}
 }
 
-/** Reset PC and set other registers to 0 */
-static void reset_cpu()
+/** Find memory region of an address */
+static MemoryRegion * find_mem_region(uint32_t *address) {
+    for (i = 0; i < NB_REGIONS; i++) {
+        if (mem_region[i].start <= address &&
+            address < (mem_region[i].start + mem_region[i].size)) {
+            return &mem_region[i];
+        }
+    }
+    return NULL;
+}
+
+/** Write 32-bit data to address (Big-Endian) */
+static void mem_write_32(uint32_t *address, uint32_t data)
 {
-	int i;
-	cpu_state.CPSR = 0x00;
-	for (i = 0; i < NB_REGS; i++) {
-		cpu_state.regs[i] = 0x00;	
-	}
+    region = find_mem_region(address);
+    assert(region != NULL);
+    uint32_t offset = address - region->start;
+    *((uint32_t *) (region->mem + offset)) = data;
+}
+
+/** Read 32-bit data from address (Big-Endian) */
+static uint32_t mem_read_32(uint32_t *address)
+{
+    region = find_mem_region(address);
+    assert(region != NULL);
+    uint32_t offset = address - region->start;
+    uint32_t data = *((uint32_t *) (region->mem + offset));
+    return data;
 }
