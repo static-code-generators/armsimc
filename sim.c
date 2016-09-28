91af26dc55d1267ea8ff0c495c9ba4ca98420a44
Jai Luthra <me@jailuthra.in>
2016-09-29 00:41:17 +0530

    sim: add sim.h, make compilable

--- sim.c
+++ sim.c
@@ -1,30 +1,9 @@
 #include <stdint.h>
 #include <stdlib.h>
-
-#define NB_REGS 16
-
-struct CPUState {
-	uint32_t regs[NB_REGS]; ///> Register File
-	uint32_t CPSR; ///> Current Program Status Register
-};
-
-// We are not implementing stack ops right now
-// The values below have been chosen arbitrarily
-#define MEM_TEXT_START 0x00000000
-#define MEM_TEXT_SIZE  0x00100000
-#define MEM_DATA_START 0x20000000
-#define MEM_DATA_SIZE  0x00100000
-
-#define NB_REGIONS 2
-#define MEM_TEXT 0
-#define MEM_DATA 1
-
-struct MemoryRegion {
-	uint32_t start;
-	uint32_t size;
-	uint8_t *mem;
-};
-
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include "sim.h"
 
 /* Global state variables */
 struct CPUState state_current, state_next;
@@ -45,17 +24,6 @@ void initialize()
     state_next = state_current;
 }
 
-/** Load program into memory */
-void load_program(FILE *code)
-{
-    uint32_t word;
-    int i = 0;
-    while (fscanf(code, "%x\n", &word) != EOF) {
-        mem_write_32(MEM_TEXT_START + i, word);
-        i += 4;
-    }
-}
-
 /** Set all registers to 0 */
 void reset_cpu()
 {
@@ -67,8 +35,8 @@ void reset_cpu()
 }
 
 /** Find memory region of an address */
-static MemoryRegion * find_mem_region(uint32_t *address) {
-    for (i = 0; i < NB_REGIONS; i++) {
+static struct MemoryRegion * find_mem_region(uint32_t address) {
+    for (int i = 0; i < NB_REGIONS; i++) {
         if (mem_region[i].start <= address &&
             address < (mem_region[i].start + mem_region[i].size)) {
             return &mem_region[i];
@@ -78,20 +46,31 @@ static MemoryRegion * find_mem_region(uint32_t *address) {
 }
 
 /** Write 32-bit data to address (Big-Endian) */
-static void mem_write_32(uint32_t *address, uint32_t data)
+static void mem_write_32(uint32_t address, uint32_t data)
 {
-    region = find_mem_region(address);
+    struct MemoryRegion *region = find_mem_region(address);
     assert(region != NULL);
     uint32_t offset = address - region->start;
     *((uint32_t *) (region->mem + offset)) = data;
 }
 
 /** Read 32-bit data from address (Big-Endian) */
-static uint32_t mem_read_32(uint32_t *address)
+static uint32_t mem_read_32(uint32_t address)
 {
-    region = find_mem_region(address);
+    struct MemoryRegion *region = find_mem_region(address);
     assert(region != NULL);
     uint32_t offset = address - region->start;
     uint32_t data = *((uint32_t *) (region->mem + offset));
     return data;
+}
+
+/** Load program into memory */
+void load_program(FILE *code)
+{
+    uint32_t word;
+    int i = 0;
+    while (fscanf(code, "%x\n", &word) != EOF) {
+        mem_write_32(MEM_TEXT_START + i, word);
+        i += 4;
+    }
 }
---
+++ sim.h
@@ -0,0 +1,32 @@
+#ifndef SIM_H
+#define SIM_H
+
+#define NB_REGS 16
+
+struct CPUState {
+	uint32_t regs[NB_REGS]; ///> Register File
+	uint32_t CPSR; ///> Current Program Status Register
+};
+
+// We are not implementing stack ops right now
+// The values below have been chosen arbitrarily
+#define MEM_TEXT_START 0x00000000
+#define MEM_TEXT_SIZE  0x00100000
+#define MEM_DATA_START 0x20000000
+#define MEM_DATA_SIZE  0x00100000
+
+#define NB_REGIONS 2
+#define MEM_TEXT 0
+#define MEM_DATA 1
+
+struct MemoryRegion {
+	uint32_t start;
+	uint32_t size;
+	uint8_t *mem;
+};
+
+void initialize();
+void load_program(FILE *code);
+void reset_cpu();
+
+#endif
