27dd81ebc50cd158e32e829e887ed675e98801dd
Jai Luthra <me@jailuthra.in>
2016-10-04 00:43:24 +0530

    isa: ldr str and strb, also get_bits

--- isa.c
+++ isa.c
@@ -15,9 +15,58 @@ static struct CPUState next_state, curr_state;
 struct CPUState process_instruction(struct CPUState state)
 {
     next_state = curr_state = state;
+    uint32_t instruction = mem_read_32(curr_state.regs[PC]);
+    decode_and_exec(instruction);
     next_state.regs[PC] += 4;
-    // TODO: decode instruction @ PC and execute it
     return next_state;
+}
+
+static void decode_and_exec(uint32_t instruction)
+{
+    if (get_bits(instruction, 27, 25) == 0x2 ||
+        (get_bits(instruction, 27, 25) ==  0x3 && get_bit(instruction, 4) == 0)) {
+        // LOAD STORE INSTRUCTIONS
+        if (get_bit(instruction, B_BIT) == 0) {
+            if (get_bit(instruction, L_BIT) == 1) { // LDR
+                exec_LDR(instruction);
+            } else { // STR
+                exec_STR(instruction);
+            }
+        } else {
+            if (get_bit(instruction, L_BIT) == 1) { // LDRB
+                // @RadhikaG has to do this
+            } else { // STRB
+                exec_STRB(instruction);
+            }
+        }
+    }
+}
+
+static void exec_LDR(uint32_t instruction)
+{
+    if (condition_check(get_bits(instruction, 31, 28))) {
+        uint32_t data = mem_read_32(ld_str_addr_mode(instruction));
+        uint32_t rd_id = get_bits(instruction, 15, 12);
+        next_state.regs[rd_id] = data;
+    }
+}
+
+static void exec_STR(uint32_t instruction)
+{
+    if (condition_check(get_bits(instruction, 31, 28))) {
+        uint32_t rd_id = get_bits(instruction, 15, 12);
+        uint32_t data = curr_state.regs[rd_id];
+        mem_write_32(ld_str_addr_mode(instruction), data);
+    }
+}
+
+static void exec_STRB(uint32_t instruction)
+{
+    if (condition_check(get_bits(instruction, 31, 28))) {
+        uint32_t rd_id = get_bits(instruction, 15, 12);
+        uint8_t data = curr_state.regs[rd_id] & 0xff; // LSB byte of reg
+        mem_write_8(ld_str_addr_mode(instruction), data);
+    }
 }
 
 static uint32_t rotate_right(uint32_t shiftee, uint8_t shifter)
@@ -29,6 +78,14 @@ static uint8_t get_bit(uint32_t from, uint8_t bitid)
 {
     return (from >> bitid) & 1;
 }
+
+static uint32_t get_bits(uint32_t from, uint8_t msb_id, uint8_t lsb_id)
+{
+    uint8_t nb_bits = msb_id - lsb_id + 1;
+    uint32_t mask = (1 << nb_bits) - 1;
+    return (from >> lsb_id) & mask;
+}
+
 
 static struct ShifterOperand * shifter_operand(struct CPUState state, uint32_t instruction)
 {
